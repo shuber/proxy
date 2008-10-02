9226195157ab2f55f34c2fb16ca3849d734f3625
Sean Huber <shuber@me.com>
2008-10-01 19:21:02 -0700

    Reordered requires and inclusions in proxy.rb

--- lib/proxy.rb
+++ lib/proxy.rb
@@ -1,7 +1,7 @@
-require 'huberry/action_controller/dispatcher'
-require 'huberry/action_controller/base'
 require 'huberry/action_controller/abstract_request'
+require 'huberry/action_controller/base'
+require 'huberry/action_controller/dispatcher'
 
-::ActionController::Dispatcher.send :include, ::Huberry::ActionController::Dispatcher
-::ActionController::Base.send :include, ::Huberry::ActionController::Base
 ::ActionController::AbstractRequest.send :include, ::Huberry::ActionController::AbstractRequest
+::ActionController::Base.send :include, ::Huberry::ActionController::Base
+::ActionController::Dispatcher.send :include, ::Huberry::ActionController::Dispatcher
