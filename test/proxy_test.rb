84e631118bcc7f68cb92a249640996aaa6378e4d
solojavier <1.27201@gmail.com>
2013-06-26 17:08:48 -0500

    Adjusting tests

--- test/proxy_test.rb
+++ test/proxy_test.rb
@@ -7,28 +7,26 @@ class ProxyTest < Test::Unit::TestCase
     @request    = ActionController::TestRequest.new
     def @request.host; env['HTTP_X_FORWARDED_HOST'].blank? ? env['HTTP_HOST'] : env['HTTP_X_FORWARDED_HOST'].split(/,\s/).last; end
     @response   = ActionController::TestResponse.new
-    @dispatcher = ActionController::Dispatcher.new(StringIO.new)
+    @dispatcher = ActionDispatch::Routing::RouteSet::Dispatcher.new({})
     @dispatcher.instance_variable_set('@request', @request)
-    ActionController::UrlWriter.default_url_options[:host] = nil
-    ActionController::Base.relative_url_root = nil
-    
+
     Proxy.replace_host_with do |request|
       'replaced.com' if request.host == 'replace-me.com'
     end
   end
-  
+
   def test_should_not_replace_host
     @request.env['HTTP_HOST'] = 'dont-replace-me.com'
     assert_equal 'dont-replace-me.com', @request.host
     Proxy.send(:before_dispatch, @dispatcher)
     assert_equal 'dont-replace-me.com', @request.host
   end
-  
+
   def test_should_replace_host
     @request.env['HTTP_HOST'] = 'replace-me.com'
     assert_equal 'replace-me.com', @request.host
     Proxy.send(:before_dispatch, @dispatcher)
     assert_equal 'replaced.com', @request.host
   end
-  
+
 end
