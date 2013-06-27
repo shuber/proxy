a91eaaac97effeab2f14a74038b6c326a663bf02
solojavier <1.27201@gmail.com>
2013-06-27 13:03:38 -0500

    Setting Proxy as a middleware for rails 3

--- lib/proxy.rb
+++ lib/proxy.rb
@@ -26,10 +26,41 @@ module Proxy
     end
 end
 
-ActionDispatch::Callbacks.before do |dispatcher|
-  Proxy.send :before_dispatch, dispatcher
+module Proxy
+  class Middleware
+    mattr_accessor :replace_host_with_proc
+    self.replace_host_with_proc = proc { |request| }
+
+    def self.replace_host_with(&block)
+      self.replace_host_with_proc = block
+    end
+
+    def initialize(app)
+      @app = app
+    end
+
+    def call(env)
+      request  = Rack::Request.new(env)
+      new_host = replace_host_with_proc.call(request)
+      if /([^\.]+\.[^\.]+)$/.match(request.host)
+        original_host = ".#{$1}"
+      else
+        original_host = request.host
+      end
+      session_options = request.env['rack.session.options']
+      request.env['rack.session.options'].merge!(:domain => original_host) if session_options# force cookie that matches original domain without subdomain
+      request.env['HTTP_X_FORWARDED_HOST'] = [request.host, new_host].join(', ') unless new_host.blank?
+
+      @app.call(env)
+    end
+  end
+
+  class Railtie < Rails::Railtie
+    initializer "proxy.initializer" do |app|
+      app.config.middleware.use "Proxy::Middleware"
+    end
+  end
 end
 
-ActionDispatch::Request = ActionDispatch::Request if defined?(ActionDispatch::Request)
 ActionDispatch::Request.send :include, Proxy::ActionDispatch::Request
 ActionController::Base.send :include, Proxy::ActionController::Base
